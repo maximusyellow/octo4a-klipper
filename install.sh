cc96245ee8da7f8edc16be7817ce0dadc17fae46
Louis Rousseau <louisrousseau@gmail.com>
2023-07-28 22:19:42 -0400

    Added GNU grep package

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ sudo apk add git unzip libffi-dev make gcc g++ \
 ncurses-dev avrdude gcc-avr binutils-avr avr-libc \
 python3 py3-virtualenv \
 python3-dev freetype-dev fribidi-dev harfbuzz-dev jpeg-dev lcms2-dev openjpeg-dev tcl-dev tiff-dev tk-dev zlib-dev \
-jq patch libsodium caddy curl polkit
+jq patch libsodium caddy curl polkit grep
 
 case $CLIENT in
   fluidd)
